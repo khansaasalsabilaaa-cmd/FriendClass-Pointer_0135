60bb927a91ab16b863f715e841e69c7df73bff17
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-04 22:45:11 +0700

    menambahkan rumus input belah ketupat

--- FriendClass&Pointer.cpp
+++ FriendClass&Pointer.cpp
@@ -43,6 +43,17 @@ class BelahKetupat {
 private:
     float d1, d2;
     float sisi;
+
+public:
+    void input(){
+        cout << "\n=== Input Belah Ketupat ===" << endl;
+        cout << "Diagonal 1 : ";
+        cin >> d1;
+        cout << "Diagonal 2 : ";
+        cin >> d2;
+        cout << "Sisi : ";
+        cin >> sisi;
+    }
     
 }
 
