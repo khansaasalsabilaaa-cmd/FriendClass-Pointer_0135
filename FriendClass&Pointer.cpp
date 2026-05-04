5ff58187641581be6a6556db01d022640ed63faa
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-04 22:24:25 +0700

    menambahkan rumus input layang layang

--- FriendClass&Pointer.cpp
+++ FriendClass&Pointer.cpp
@@ -7,5 +7,20 @@ class LayangLayang {
 private:
     float d1, d2; //diagonal
     float a, b; // sisi
+
+public:
+    void input(){
+        cout << "=== Input Layang-Layang ===" << endl;
+        cout << "Diagonal 1 : "; 
+        cin >> d1;
+        cout << "Diagonal 2 : ";
+        cin >> d2;
+        cout << "Sisi a : ";
+        cin >> a;
+        cout << "Sisi b : ";
+        cin >> b;
+    }
+    
+
 }
 
